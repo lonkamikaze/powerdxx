7bdf55bd388362128805448f58cbe21fa49e9fe3
Dominic Fandrey <kami@freebsd.org>
2016-02-29 00:11:52 +0100

    Add install infrastructure for FreeBSD ports
    Adds install.sh, a script that expects a table of install instructions
    on stdin. Adds pkg.tbl, said table and the Makefile hooks to make
    it work.

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
 CXXFLAGS+= -std=c++11 -Wall -Werror
 
+all: powerd++
+
 powerd++: powerd++.o
 	${CXX} -lutil ${CXXFLAGS} -o ${.TARGET} ${.ALLSRC}
 
 powerd++.o: src/powerd++.cpp src/Options.hpp
 
+.if defined(SUB)
+install: install.sh pkg.tbl powerd++
+	@sed ${SUB} pkg.tbl | ./install.sh DESTDIR="${DESTDIR}"
+.endif
---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -f
+export "$@"
+IFS=':'
+while read type src tgt; do
+	test -n "${NOPORTDOCS}" -a "${type}" == "MAN" && continue
+	eval echo "\${BSD_INSTALL_${type}} ${src} ${DESTDIR:+${DESTDIR%/}/}${tgt}"
+	mkdir -p ${DESTDIR:+${DESTDIR%/}/}${tgt%/*}
+	eval $(eval echo "\${BSD_INSTALL_${type}} ${src} ${DESTDIR:+${DESTDIR%/}/}${tgt%.gz}")
+	case ${type} in
+	MAN | SCRIPT)
+		eval "sed -i '' ${SUB} ${DESTDIR:+${DESTDIR%/}/}${tgt%.gz}"
+	;;
+	esac
+done
