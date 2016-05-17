b27a84c30866e18b9c2195676e8ca4a59584c99d
Dominic Fandrey <kami@freebsd.org>
2016-05-17 18:55:51 +0200

    Make `make install` functional outside of ports
    The following changes make it easier to make a test install with
    a not yet released version.
    
    - Makefile
      - Add default PREFIX and DOCSDIR
    - install.sh
      - Add default BSD_INSTALL_*
      - Readd support for gz'ing and a default GZIP_CMD
    - pkg.tbl
      - Compress manual page

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 CXXFLAGS+= -std=c++11 -Wall -Werror
+PREFIX?=   /usr/local
+DOCSDIR?=  ${PREFIX}/share/doc/powerdxx
 
 all: powerd++
 
@@ -7,9 +9,7 @@ powerd++: powerd++.o
 
 powerd++.o: src/powerd++.cpp src/Options.hpp
 
-.if defined(PREFIX) && defined(DOCSDIR)
 install: install.sh pkg.tbl powerd++
 	@${.CURDIR}/install.sh < ${.CURDIR}/pkg.tbl \
 		DESTDIR="${DESTDIR}" PREFIX="${PREFIX}" DOCSDIR="${DOCSDIR}" \
 		CURDIR="${.CURDIR}" OBJDIR="${.OBJDIR}"
-.endif
--- install.sh
+++ install.sh
@@ -6,13 +6,20 @@ for envvar in CURDIR OBJDIR PREFIX DOCSDIR; do
 	eval "sedprog=\"${sedprog}${sedprog:+;}s!%%${envvar}%%!\${${envvar}%/}!g\""
 done
 IFS=':'
+: ${BSD_INSTALL_PROGRAM=install  -s -m 555}
+: ${BSD_INSTALL_MAN=install  -m 444}
+: ${BSD_INSTALL_SCRIPT=install  -m 555}
+: ${GZIP_CMD=gzip -nf9}
 sed "${sedprog}" | while read type src tgt; do
 	eval echo "\${BSD_INSTALL_${type}} ${src} ${DESTDIR:+${DESTDIR%/}/}${tgt}"
 	mkdir -p ${DESTDIR:+${DESTDIR%/}/}${tgt%/*}
-	eval $(eval echo "\${BSD_INSTALL_${type}} ${src} ${DESTDIR:+${DESTDIR%/}/}${tgt}")
+	eval eval "\${BSD_INSTALL_${type}} ${src} ${DESTDIR:+${DESTDIR%/}/}${tgt%.gz}"
 	case ${type} in
 	MAN | SCRIPT)
-		sed -i '' "${sedprog}" "${DESTDIR:+${DESTDIR%/}/}${tgt}"
+		sed -i '' "${sedprog}" "${DESTDIR:+${DESTDIR%/}/}${tgt%.gz}"
 	;;
 	esac
+	if [ -z "${tgt##*.gz}" ]; then
+		eval ${GZIP_CMD} "${DESTDIR:+${DESTDIR%/}/}${tgt%.gz}"
+	fi
 done
