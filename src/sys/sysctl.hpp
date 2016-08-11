8b4085182f3151e1663fc2fee4dbcd46df5c944a
Dominic Fandrey <kami@freebsd.org>
2016-08-11 23:12:57 +0200

    Document and rename Sysctl::update() to get()

--- src/sys/sysctl.hpp
+++ src/sys/sysctl.hpp
@@ -28,8 +28,25 @@ typedef int mib_t;
 /**
  * Represents a sysctl MIB address.
  *
+ * It offers set() and get() methods to access these sysctls.
+ *
+ * There are two ways of initialising a Sysctl instance, by symbolic
+ * name or by directly using the MIB address. The latter one only
+ * makes sense for sysctls with a fixed address, known at compile
+ * time, e.g. `Sysctl<2>{CTL_HW, HW_NCPU}` for "hw.ncpu". Check
+ * `/usr/include/sys/sysctl.h` for predefined MIBs.
+ *
+ * For all other sysctls, symbolic names must be used. E.g.
+ * `Sysctl<4>{"dev.cpu.0.freq"}`. Creating a Sysctl from a symbolic
+ * name may throw.
+ *
+ * A Sysctl instance created with the default constructor is unitialised,
+ * initialisation can be deferred to a later moment by using copy assignment.
+ * This can be used to create globals but construct them inline where
+ * exceptions can be handled.
+ *
  * @tparam MibDepth
- *	The maximum allowed MIB depth
+ *	The MIB level, e.g. "hw.ncpu" is two levels deep
  */
 template <size_t MibDepth>
 class Sysctl {
@@ -53,7 +70,7 @@ class Sysctl {
 	 * Initialise the MIB address from a character string.
 	 *
 	 * @param name
-	 *	The name of the sysctl
+	 *	The symbolic name of the sysctl
 	 * @throws sc_error
 	 *	May throw an exception if the addressed sysct does
 	 *	not exist or if the address is too long to store
@@ -63,6 +80,7 @@ class Sysctl {
 		if (::sysctlnametomib(name, this->mib, &length) == -1) {
 			throw sc_error{errno};
 		}
+		assert(length == MibDepth && "MIB depth mismatch");
 	}
 
 	/**
@@ -84,8 +102,8 @@ class Sysctl {
 	template <typename... Tail>
 	constexpr Sysctl(mib_t const head, Tail const... tail) noexcept :
 	    mib{head, tail...} {
-		static_assert(MibDepth >= sizeof...(Tail) + 1,
-		              "The number of MIB addresses must not exceed the MIB depth");
+		static_assert(MibDepth == sizeof...(Tail) + 1,
+		              "MIB depth mismatch");
 	}
 
 	/**
@@ -99,7 +117,7 @@ class Sysctl {
 	 *	e.g. because the value does not fit into the target
 	 *	buffer
 	 */
-	void update(void * const buf, size_t const bufsize) const {
+	void get(void * const buf, size_t const bufsize) const {
 		auto len = bufsize;
 		if (::sysctl(this->mib, MibDepth, buf, &len, nullptr, 0)
 		    == -1) {
@@ -121,8 +139,8 @@ class Sysctl {
 	 *	type
 	 */
 	template <typename T>
-	void update(T & value) const {
-		update(&value, sizeof(T));
+	void get(T & value) const {
+		get(&value, sizeof(T));
 	}
 
 	/**
@@ -148,7 +166,7 @@ class Sysctl {
 			throw sc_error{errno};
 		}
 		auto result = std::unique_ptr<T[]>(new T[len / sizeof(T)]);
-		update(result.get(), len);
+		get(result.get(), len);
 		return result;
 	}
 
@@ -179,59 +197,192 @@ class Sysctl {
 	void set(T const & value) {
 		set(&value, sizeof(T));
 	}
-
 };
 
+/**
+ * This is a wrapper around Sysctl that allows semantically transparent
+ * use of a sysctl.
+ *
+ * ~~~ c++
+ * Sync<int, Sysctl<3>> sndUnit{{"hw.snd.default_unit"}};
+ * if (sndUnit != 3) {    // read from sysctl
+ *	sndUnit = 3;      // assign to sysctl
+ * }
+ * ~~~
+ *
+ * Note that both assignment and read access (implemented through
+ * type casting to T) may throw an exception.
+ *
+ * @tparam T
+ *	The type to represent the sysctl as
+ * @tparam SysctlT
+ *	The Sysctl type
+ */
 template <typename T, class SysctlT>
 class Sync {
 	private:
+	/**
+	 * A sysctl to represent.
+	 */
 	SysctlT sysctl;
 
 	public:
+	/**
+	 * The constructor copies the given Sysctl instance.
+	 *
+	 * @param sysctl
+	 *	The Sysctl instance to represent
+	 */
 	constexpr Sync(SysctlT const & sysctl) noexcept : sysctl{sysctl} {}
 
+	/**
+	 * Transparently assiges values of type T to the represented
+	 * Sysctl instance.
+	 *
+	 * @param value
+	 *	The value to assign
+	 * @return
+	 *	A self reference
+	 */
 	Sync & operator =(T const & value) {
 		this->sysctl.set(value);
 		return *this;
 	}
 
+	/**
+	 * Implicitly cast to the represented type.
+	 *
+	 * @return
+	 *	Returns the value from the sysctl
+	 */
 	operator T () const {
 		T value;
-		this->sysctl.update(value);
+		this->sysctl.get(value);
 		return value;
 	}
 };
 
+/**
+ * A convenience alias around Sync.
+ *
+ * ~~~ c++
+ * // Sync<int, Sysctl<3>> sndUnit{{"hw.snd.default_unit"}};
+ * SysctlSync<int, 3> sndUnit{{"hw.snd.default_unit"}};
+ * if (sndUnit != 3) {    // read from sysctl
+ *	sndUnit = 3;      // assign to sysctl
+ * }
+ * ~~~
+ *
+ * @tparam T
+ *	The type to represent the sysctl as
+ * @tparam MibDepth
+ *	The maximum allowed MIB depth
+ */
 template <typename T, size_t MibDepth>
 using SysctlSync = Sync<T, Sysctl<MibDepth>>;
 
+/**
+ * A read once representation of a Sysctl.
+ *
+ * This reads a sysctl once upon construction and always returns that
+ * value. It does not support assignment.
+ *
+ * This class is intended for sysctls that are not expected to change,
+ * such as hw.ncpu. A special property of this class is that the
+ * constructor does not throw and takes a default value in case reading
+ * the sysctl fails.
+ *
+ * ~~~ c++
+ * // Read number of CPU cores, assume 1 on failure:
+ * Once<coreid_t, Sysctl<2>> ncpu{1, {CTL_HW, HW_NCPU}};
+ * // Equivalent:
+ * int hw_ncpu;
+ * try {
+ * 	Sysctl<2>{CTL_HW, HW_NCPU}.get(hw_ncpu);
+ * } catch (sys::sc_error) {
+ * 	hw_ncpu = 1;
+ * }
+ * ~~~
+ *
+ * @tparam T
+ *	The type to represent the sysctl as
+ * @tparam SysctlT
+ *	The Sysctl type
+ */
 template <typename T, class SysctlT>
 class Once {
 	private:
+	/**
+	 * The sysctl value read upon construction.
+	 */
 	T value;
 
 	public:
+	/**
+	 * The constructor tries to read and store the requested sysctl.
+	 *
+	 * If reading the requested sysctl fails for any reason,
+	 * the given value is stored instead.
+	 *
+	 * @param value
+	 *	The fallback value
+	 * @param sysctl
+	 *	The sysctl to represent
+	 */
 	Once(T const & value, SysctlT const & sysctl) noexcept {
 		try {
-			sysctl.update(this->value);
+			sysctl.get(this->value);
 		} catch (sc_error) {
 			this->value = value;
 		}
 	}
 
+	/**
+	 * Return a const reference to the value.
+	 *
+	 * @return
+	 *	A const reference to the value
+	 */
 	operator T const &() const {
 		return this->value;
 	}
 };
 
+/**
+ * A convenience alias around Once.
+ *
+ * ~~~ c++
+ * // Once<coreid_t, Sysctl<2>> ncpu{0, {CTL_HW, HW_NCPU}};
+ * SysctlOnce<coreid_t, 2> ncpu{1, {CTL_HW, HW_NCPU}};
+ * ~~~
+ *
+ * @tparam T
+ *	The type to represent the sysctl as
+ * @tparam MibDepth
+ *	The maximum allowed MIB depth
+ */
 template <typename T, size_t MibDepth>
 using SysctlOnce = Once<T, Sysctl<MibDepth>>;
 
+/**
+ * This creates a Once instance.
+ *
+ * This is intended for cases when a Once instance is created as a
+ * temporary to retrieve a value, using it's fallback to a default
+ * mechanism.
+ *
+ * @tparam T
+ *	The value type
+ * @tparam SysctlT
+ *	The Sysctl type
+ * @param value
+ *	The default value to fall back to
+ * @param sysctl
+ *	The sysctl to try and read from
+ */
 template <typename T, class SysctlT>
 constexpr Once<T, SysctlT> once(T const & value, SysctlT const & sysctl)
-    noexcept {
-	return {value, sysctl};
-}
+noexcept { return {value, sysctl}; }
 
 } /* namespace ctl */
 } /* namespace sys */
