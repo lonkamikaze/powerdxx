f1e42b315d4d3c340543af4379b9726073f4dafb
Dominic Fandrey <kami@freebsd.org>
2016-08-18 22:34:14 +0200

    Remove outsourced utility functions

--- src/powerd++.cpp
+++ src/powerd++.cpp
@@ -77,18 +77,6 @@ enum class AcLineState : unsigned int {
 const char * const AcLineStateStr[]{"battery", "online", "unknown"};
 
 /**
- * Like sizeof(), but it returns the number of elements an array consists
- * of instead of the number of bytes.
- *
- * @tparam T,Count
- *	The type and number of array elements
- * @return
- *	The number of array entries
- */
-template <typename T, size_t Count>
-constexpr size_t countof(T (&)[Count]) { return Count; }
-
-/**
  * Contains the management information for a single CPU core.
  */
 struct Core {
@@ -252,45 +240,6 @@ enum class Unit : size_t {
 char const * const UnitStr[]{
 	"", "%", "s", "ms", "hz", "khz", "mhz", "ghz", "thz"
 };
-
-/**
- * A string literal operator equivalent to the `operator "" s` literal
- * provided by C++14 in \<string\>.
- *
- * @param op
- *	The raw string to turn into an std::string object
- * @param size
- *	The size of the raw string
- * @return
- *	An std::string instance
- */
-std::string operator "" _s(char const * const op, size_t const size) {
-	return {op, size};
-}
-
-/**
- * A wrapper around snprintf() that automatically pulls in the
- * destination buffer size.
- *
- * @tparam Size
- *	The destination buffer size
- * @tparam Args
- *	The types of the arguments
- * @param dst
- *	A reference to the destination buffer
- * @param format
- *	A printf style formatting string
- * @param args
- *	The printf arguments
- * @return
- *	The number of characters in the resulting string, regardless of the
- *	available space
- */
-template <size_t Size, typename... Args>
-inline int sprintf(char (& dst)[Size], const char * const format,
-                   Args const... args) {
-	return snprintf(dst, Size, format, args...);
-}
 
 /**
  * Determine the unit of a string encoded value.
