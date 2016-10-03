1160a0e542322b04d181d0a5e89f56c85b3c7a9a
Dominic Fandrey <kami@freebsd.org>
2016-10-03 22:42:31 +0200

    Add shell script for executing load replays

---
+++ loadplay.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+set -f
+preload="%%PREFIX%%/lib/libloadplay.so"
+while [ "${1#-}" != "$1" ]; do
+	case "$1" in
+	-h | --help)
+		/bin/cat 1>&2 << USAGE
+usage: loadplay [-h] [-i file] [-o file] command ...
+
+        -h, --help          Show usage and exit
+        -i, --input   file  Input from file
+        -o, --output  file  Output to file
+USAGE
+		return 0
+	;;
+	-i | --input)
+		exec <"$2"
+		shift 2
+	;;
+	-i*)
+		exec <"${1#-i}"
+		shift
+	;;
+	-o | --output)
+		exec >"$2"
+		shift 2
+	;;
+	-o*)
+		exec <"${1#-o}"
+		shift
+	;;
+	- | -? | --*)
+		echo "loadplay: unknown option: $1"
+		return 1
+	;;
+	-?*)
+		arg="$1"
+		shift
+		set -- "${arg%%${arg#-?}}" "-${arg#-?}" "$@"
+	;;
+	esac
+done
+if [ -z "$1" ]; then
+	echo "loadplay: command missing"
+	return 1
+fi
+exec /usr/bin/env LD_PRELOAD="$preload" "$@"
