3eca04660014881bd6d5deae722ae6b4ab97c5e6
Dominic Fandrey <kami@freebsd.org>
2016-10-19 20:53:55 +0200

    Split environment from installation routine

---
+++ common.sh
@@ -0,0 +1,12 @@
+set -f
+export "$@"
+sedprog=
+for envvar in CURDIR OBJDIR PREFIX DOCSDIR; do
+	eval "sedprog=\"${sedprog};s!%%${envvar}%%!\${${envvar}%/}!g\""
+done
+IFS=':'
+: ${BSD_INSTALL_PROGRAM=install  -s -m 555}
+: ${BSD_INSTALL_MAN=install  -m 444}
+: ${BSD_INSTALL_SCRIPT=install  -m 555}
+: ${BSD_INSTALL_LIB=install  -s -m 444}
+: ${GZIP_CMD=gzip -nf9}
--- install.sh
+++ install.sh
@@ -1,23 +1,12 @@
 #!/bin/sh
-set -f
-export "$@"
-sedprog=
-for envvar in CURDIR OBJDIR PREFIX DOCSDIR; do
-	eval "sedprog=\"${sedprog}${sedprog:+;}s!%%${envvar}%%!\${${envvar}%/}!g\""
-done
-IFS=':'
-: ${BSD_INSTALL_PROGRAM=install  -s -m 555}
-: ${BSD_INSTALL_MAN=install  -m 444}
-: ${BSD_INSTALL_SCRIPT=install  -m 555}
-: ${BSD_INSTALL_LIB=install  -s -m 444}
-: ${GZIP_CMD=gzip -nf9}
+. "${0%/*}/common.sh" "$@"
 sed "${sedprog}" | while read type src tgt; do
-	eval echo "\${BSD_INSTALL_${type}} ${src} ${DESTDIR:+${DESTDIR%/}/}${tgt}"
-	mkdir -p ${DESTDIR:+${DESTDIR%/}/}${tgt%/*}
-	eval eval "\${BSD_INSTALL_${type}} ${src} ${DESTDIR:+${DESTDIR%/}/}${tgt%.gz}"
+	eval echo "\${BSD_INSTALL_${type}} \"\${src}\" \"\${DESTDIR:+\${DESTDIR%/}/}\${tgt}\""
+	mkdir -p "${DESTDIR:+${DESTDIR%/}/}${tgt%/*}"
+	eval eval "\${BSD_INSTALL_${type}} \"\${src}\" \"\${DESTDIR:+\${DESTDIR%/}/}\${tgt%.gz}\""
 	case ${type} in
 	MAN | SCRIPT)
-		sed -i '' "/#RM\$/d;${sedprog}" "${DESTDIR:+${DESTDIR%/}/}${tgt%.gz}"
+		sed -i '' "/#RM\$/d${sedprog}" "${DESTDIR:+${DESTDIR%/}/}${tgt%.gz}"
 	;;
 	esac
 	if [ -z "${tgt##*.gz}" ]; then
