c5e83c307b84b6fee1e4f3cc1696060218e161bf
Dominic Fandrey <kami@freebsd.org>
2016-12-15 22:21:09 +0100

    Add printf style string formatting facilities

--- src/utility.hpp
+++ src/utility.hpp
@@ -3,6 +3,7 @@
  */
 
 #include <type_traits> /* std::underlying_type */
+#include <string>
 
 #include <cstdio>      /* snprintf() */
 
@@ -86,6 +87,68 @@ template <class ET, typename VT = typename std::underlying_type<ET>::type>
 constexpr VT to_value(ET const op) {
 	return static_cast<VT>(op);
 }
+
+/**
+ * A formatting wrapper around string literals.
+ *
+ * Overloads operator (), which treats the string as a printf formatting
+ * string, the arguments represent the data to format.
+ *
+ * In combination with the literal _fmt, it can be used like this:
+ *
+ * ~~~ c++
+ * std::cout << "%-15.15s %#018p\n"_fmt("Address:", this);
+ * ~~~
+ */
+class Formatter {
+	private:
+	/**
+	 * Pointer to the string literal.
+	 */
+	char const * const fmt;
+
+	public:
+	/**
+	 * Construct from string literal.
+	 */
+	constexpr Formatter(char const * const fmt) : fmt{fmt} {}
+
+	/**
+	 * Returns a formatted string.
+	 *
+	 * @tparam ArgTs
+	 *	Variadic argument types
+	 * @param args
+	 *	Variadic arguments
+	 * @return
+	 *	An std::string formatted according to fmt
+	 */
+	template <typename... ArgTs>
+	std::string operator ()(ArgTs const &... args) const {
+		char buf[65536];
+		auto count = sprintf(buf, this->fmt, args...);
+		if (count >= countof(buf)) {
+			return {buf, countof(buf) - 1};
+		} else if (count < 0) {
+			return {};
+		}
+		return {buf, static_cast<size_t>(count)};
+	}
+};
+
+namespace literals {
+/**
+ * Literal to convert a string literal to a Formatter instance.
+ *
+ * @param fmt
+ *	A printf style format string
+ * @return
+ *	A Formatter instance
+ */
+constexpr Formatter operator "" _fmt(char const * const fmt, size_t const) {
+	return {fmt};
+}
+} /* namespace literals */
 
 } /* namespace utility */
 
