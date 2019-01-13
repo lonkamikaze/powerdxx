398780680270b619293936baa1da2ec3b1c5f90f
Dominic Fandrey <kami@freebsd.org>
2019-01-13 21:14:53 +0100

    Remove obsolete \htmlonly
    The \htmlonly was inserted, because without it <u> does not work,
    however it causes the manual to only be included in the HTML documentation.
    
    Doxygen 1.8.15 will contain a fix for this issue, making this workaround
    obsolete.

--- doxy/mandoc.sh
+++ doxy/mandoc.sh
@@ -6,9 +6,7 @@ name="${name%.*}"
 sane="$(echo "$name" | tr '+' 'x')"
 sect="${1##*.}"
 echo "\\page man_${sect}_$sane $name($sect) Manual
-
-\htmlonly"
+"
 awk '{for (name in ENVIRON) gsub("%%" name "%%", ENVIRON[name])}1' "$1" | \
                                             mandoc -Kutf-8 -Tutf8 -mdoc | \
                                                 "${0%/*}/mantohtml.awk"
-echo '\endhtmlonly'
