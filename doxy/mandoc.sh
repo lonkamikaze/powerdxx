7d41f53760d2f627c147143ae3a7acccad2450b4
Dominic Fandrey <kami@freebsd.org>
2019-01-16 22:02:01 +0100

    Title "Manual name" instead of "name Manual"

--- doxy/mandoc.sh
+++ doxy/mandoc.sh
@@ -5,7 +5,7 @@ name="${1##*/}"
 name="${name%.*}"
 sane="$(echo "$name" | tr '+' 'x')"
 sect="${1##*.}"
-echo "\\page man_${sect}_$sane $name($sect) Manual
+echo "\\page man_${sect}_$sane Manual $name($sect)
 "
 awk '{for (name in ENVIRON) gsub("%%" name "%%", ENVIRON[name])}1' "$1" | \
                                             mandoc -Kutf-8 -Tutf8 -mdoc | \
